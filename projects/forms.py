eab679ce82f9263bd3e99d8bf85c0bc1b620e3af
Gor-Kirakosyan <gor.kirakosyan.jan@bk.ru>
2026-05-05 19:06:35 +0500

    Исправлены формы, шаблоны и PEP8

--- projects/forms.py
+++ projects/forms.py
@@ -8,8 +8,34 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = ['name', 'description', 'github_url', 'status']
         widgets = {
-            'status': forms.Select(choices=Project.STATUS_CHOICES),
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Название проекта',
+                'class': 'form-input',
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': 'Описание проекта',
+                'rows': 5,
+                'class': 'form-textarea',
+            }),
+            'github_url': forms.URLInput(attrs={
+                'placeholder': 'https://github.com/username/repository',
+                'class': 'form-input',
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-select',
+            }),
         }
+        labels = {
+            'name': 'Название:',
+            'description': 'Описание:',
+            'github_url': 'Ссылка на GitHub:',
+            'status': 'Статус:',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].required = True
+        self.fields['status'].required = True
 
     def clean_github_url(self):
         url = self.cleaned_data.get('github_url')
--- users/forms.py
+++ users/forms.py
@@ -10,6 +10,16 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'surname', 'email']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-input'}),
+            'surname': forms.TextInput(attrs={'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+        }
+        labels = {
+            'name': 'Имя:',
+            'surname': 'Фамилия:',
+            'email': 'Email:',
+        }
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -20,13 +30,42 @@ class RegisterForm(forms.ModelForm):
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.EmailField(label="Email")
+    username = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(attrs={'class': 'form-input'})
+    )
+    password = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.error_messages['invalid_login'] = 'Неверный email или пароль'
 
 
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'surname', 'avatar', 'about', 'phone', 'github_url']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-input'}),
+            'surname': forms.TextInput(attrs={'class': 'form-input'}),
+            'avatar': forms.ClearableFileInput(
+                attrs={'class': 'form-file', 'style': 'display: none;'}
+                ),
+            'about': forms.Textarea(attrs={'rows': 4, 'class': 'form-textarea'}),
+            'phone': forms.TextInput(attrs={'class': 'form-input'}),
+            'github_url': forms.URLInput(attrs={'class': 'form-input'}),
+        }
+        labels = {
+            'name': 'Имя:',
+            'surname': 'Фамилия:',
+            'avatar': 'Аватар:',
+            'about': 'О себе:',
+            'phone': 'Телефон:',
+            'github_url': 'GitHub:',
+        }
 
     def clean_phone(self):
         phone = self.cleaned_data.get('phone')
@@ -35,7 +74,8 @@ class EditProfileForm(forms.ModelForm):
                 phone = '+7' + phone[1:]
             if User.objects.exclude(pk=self.instance.pk).filter(phone=phone).exists():
                 raise ValidationError(
-                    "Пользователь с таким номером телефона уже существует")
+                    "Пользователь с таким номером телефона уже существует"
+                )
         return phone
 
     def clean_github_url(self):
@@ -46,4 +86,14 @@ class EditProfileForm(forms.ModelForm):
 
 
 class ChangePasswordForm(PasswordChangeForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['old_password'].widget.attrs.update(
+            {'class': 'form-input'})
+        self.fields['new_password1'].widget.attrs.update(
+            {'class': 'form-input'})
+        self.fields['new_password2'].widget.attrs.update(
+            {'class': 'form-input'})
+        self.fields['old_password'].label = 'Текущий пароль:'
+        self.fields['new_password1'].label = 'Новый пароль:'
+        self.fields['new_password2'].label = 'Подтверждение пароля:'
